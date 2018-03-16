8f867e9a5e0b244b659123a23f1b5f5faddeb93e
p-pawlowski <piotr.m.pawlowski@gmail.com>
2018-03-16 11:18:08 +0100

    Update for.sql

--- ORACLE/PL-SQL/for.sql
+++ ORACLE/PL-SQL/for.sql
@@ -1,12 +1,13 @@
-for item in (
-            select COL_1
-            from table TAB_A
-            )
-        loop
-        
-            insert into TAB_B 
-				(COL_2) 
-            values 
-				( item.COL_1 );
-
-        end loop;
+for item in 
+(
+	select 
+		COL_1
+	from  
+		TAB_A
+)
+	loop        
+		insert into TAB_B 
+			(COL_2) 
+		values 
+			(item.COL_1 );
+	end loop;
