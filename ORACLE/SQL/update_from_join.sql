51f912d8e569ac0a295014a608bf9003a0d75c57
p-pawlowski <piotr.m.pawlowski@gmail.com>
2018-03-16 15:30:12 +0100

    Update update_from_join.sql

--- ORACLE/SQL/update_from_join.sql
+++ ORACLE/SQL/update_from_join.sql
@@ -1,18 +1,19 @@
 update 
-    (
-        select 
-            P.DESCRIPTION as DESCRIPTION
-        from 
-            PRODUCT P
-        where P.ID in (
-            select 
-                P_C.PRODUCT_ID 
-            from PRODUCT_CLIENT P_C 
-                join CLIENTS C on
-           (P_C.CLIENT_ID = C.ID)
-        where
-            C.ID = 1   
-        )
+	(
+		select 
+		    P.DESCRIPTION as DESCRIPTION
+		from 
+		    PRODUCT P
+		where P.ID in 
+			(
+			    select 
+				P_C.PRODUCT_ID 
+			    from PRODUCT_CLIENT P_C 
+				join CLIENTS C on
+			   (P_C.CLIENT_ID = C.ID)
+				where
+			    C.ID = 1
+			)
 	) TO_UPDATE
 set
     TO_UPDATE.DESCRIPTION = 'DESCRIPTION';
